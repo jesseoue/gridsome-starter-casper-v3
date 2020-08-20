80b05bd8d8c966cc11086e4b8c0a2892eca784e9
Marcus Reinhardt <webstone@gmail.com>
2020-08-20 19:16:33 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -59,8 +59,8 @@ module.exports = {
   transformers: {
     remark: {
       plugins: [
-        '@jammeryhq/gridsome-remark-table-align',
-        ['@jammeryhq/gridsome-remark-classes', {
+        '@noxify/gridsome-remark-table-align',
+        ['@noxify/gridsome-remark-classes', {
           'table': 'table table-striped',
           'tableCell[align=center]': 'text-center',
           'tableCell[align=right]': 'text-right'
